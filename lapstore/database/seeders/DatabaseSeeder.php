dfa839cd22436f99f7b10c1ade36571f6d337894
ansdaultana <116364572+ansdaultana@users.noreply.github.com>
2023-08-01 13:39:15 +0500

    seeder updated

--- lapstore/database/seeders/DatabaseSeeder.php
+++ lapstore/database/seeders/DatabaseSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use App\Models\Category;
+use App\Models\Image;
+use App\Models\Product;
 use App\Models\User;
 use Illuminate\Database\Seeder;
 
@@ -37,5 +39,49 @@ class DatabaseSeeder extends Seeder
             'name'=>'Accessories',
 
         ]);
+
+        Product::create(
+            [
+                'title'=>'1st laptop',
+                'quantity'=>23,
+                'description'=>'1st laptop1st laptop1st laptop1st laptop1st laptop1st laptop1st laptop1st laptop1st laptop1st laptop1st laptop1st laptop1st laptop1st laptop1st laptop1st laptop1st laptop1st laptop1st laptop1st laptop1st laptop',
+                'category_id'=>1,
+                'price'=>1000,
+                'created_by'=>1,
+            ]
+            );
+            Product::create(
+                [
+                    'title'=>'Editted with new Image',
+                    'quantity'=>23,
+                    'description'=>'1st laptop1st laptop1st laptop1st laptop1st laptop1st laptop1st laptop1st laptop1st laptop1st laptop1st laptop1st laptop1st laptop1st laptop1st laptop1st laptop1st laptop1st laptop1st laptop1st laptop1st laptop',
+                    'category_id'=>1,
+                    'price'=>1000,
+                    'created_by'=>1,
+                ]
+                ); Product::create(
+                    [
+                        'title'=>'red dragon mouse',
+                        'quantity'=>23,
+                        'description'=>'1st laptop1st laptop1st laptop1st laptop1st laptop1st laptop1st laptop1st laptop1st laptop1st laptop1st laptop1st laptop1st laptop1st laptop1st laptop1st laptop1st laptop1st laptop1st laptop1st laptop1st laptop',
+                        'category_id'=>1,
+                        'price'=>1000,
+                        'created_by'=>1,
+                    ]
+                    );
+
+                    Image::create([
+                        'image_url'=> 'https://res.cloudinary.com/ddrivhxfq/image/upload/f_auto,q_auto/xbqscokdjitefrwzqohj',
+                        'product_id'=>1,
+                    ]);
+                    
+                    Image::create([
+                        'image_url'=> 'https://res.cloudinary.com/ddrivhxfq/image/upload/v1690140547/d4fdbjkkvxibyjx8qoep.jpg',
+                        'product_id'=>2,
+                    ]);
+                    Image::create([
+                        'image_url'=> 'https://res.cloudinary.com/ddrivhxfq/image/upload/v1690400898/i1lrywcezpqaxof9nglr.webp',
+                        'product_id'=>3,
+                    ]);
     }
 }
