9f0f085d17650dab680479dcdf883caadb818dde
ansdaultana <116364572+ansdaultana@users.noreply.github.com>
2023-08-18 10:01:04 +0500

    add clear filter button

--- lapstore/resources/views/Products.blade.php
+++ lapstore/resources/views/Products.blade.php
@@ -16,8 +16,20 @@
         </div>
         <div class="flex">
             @include('button-for-product-page',['filter'=>'Sort By'])
+            <a href="{{ route('products.category', ['slug' => $category_name, 'search' => request('search')]) }}">
 
-        </div>
+            <div
+                class="bg-orange-400 text-white  rounded-full  p-1 transition-transform hover:scale-103 hover:cursor-pointer flex items-center justify-center ml-2 ">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none"
+                    viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+                    class="w-6 h-6">
+                    <path stroke-linecap="round" stroke-linejoin="round"
+                        d="M6 18L18 6M6 6l12 12" />
+                </svg>
+
+            </div>
+        </a>
+     </div>
     </div>
     <div class="flex ">
         <h1 class="text-2xl text-black p-4 ml-8 -mb-2 "> {{$category_name}} for
