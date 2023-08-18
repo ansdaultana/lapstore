da7b5d481cbd6e693aa27b4301dd86246ed92b39
ansdaultana <116364572+ansdaultana@users.noreply.github.com>
2023-08-18 11:22:22 +0500

    favourite functionilty in product view page

--- lapstore/resources/views/product.blade.php
+++ lapstore/resources/views/product.blade.php
@@ -31,9 +31,7 @@
         <div class="w-1/2 h-full ">
             <div class="mt-8  p-2 md:pl-8 flex">
                 <h1 class="text-2xl font-bold ">{{$product->title}}</h1>
-                <!-- <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="text-red-400 hover:fill-red-600 w-6 h-6 m-1 ml-7">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z" />
-                  </svg> -->
+                
             </div>
             <div class=" pl-4 md:px-8 md:py-2 flex">
                 <h1 class="text-md">{{$product->description}}</h1>
@@ -77,6 +75,15 @@
                         Left!</div>
                     <div class="hidden md:block">Don't Miss</div>
                 </div>
+                <form action="/favourite/add/{{$product->slug}}" method="POST">
+                    @csrf
+                <button class="mt-2" type="submit">
+
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class=" animate-bounce text-red-400 hover:fill-red-600 w-6 h-6 m-1 ml-7">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z" />
+                  </svg>   
+                </button>
+            </form>
             </div>
 
             <div class="md:px-4 md:py-2 mb-2 py-4 flex space-x-3 md:space-x-6">
